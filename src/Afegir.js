ddf54a12464c28b228b07a507a204072d300071b
jordi3d <jordi@cine3d.com>
2022-03-26 17:41:07 +0100

    tot funcionant

--- src/Afegir.js
+++ src/Afegir.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { useRef } from "react";
 
-export default function Afegir() {
+const ENDPOINT = "https://tc-todo-2022.herokuapp.com/todos";
+
+export default function Afegir({ onTascaAfegida }) {
   const inputRef = useRef();
-  function novaentrada(titol) {
-    console.log(titol);
-    fetch("https://tc-todo-2022.herokuapp.com/todos", {
-      method: "POST",
-      body: JSON.stringify({
-        title: titol,
-        details: "no hase falta desir nada más",
-        completed: false,
-      }),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }
   return (
-    <div>
-      <label>Títol de la Nova tasca:&nbsp;</label>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const titol = inputRef.current.value;
+        inputRef.current.value = "";
+        fetch(ENDPOINT, {
+          method: "POST",
+          body: JSON.stringify({
+            title: titol,
+            details: "no hase falta desir nada más",
+            completed: false,
+          }),
+          headers: { "Content-type": "application/json; charset=UTF-8" },
+        })
+          .then((response) => response.json())
+          .then((json) => onTascaAfegida(json));
+      }}
+    >
+      <label>Nova tasca:&nbsp;</label>
       <input type="text" id="titol" ref={inputRef} />
-      <br />
-      <input
-        type="submit"
-        value="Afegir"
-        onClick={() => novaentrada(inputRef.current.value)}
-      />
-    </div>
+      <input type="submit" value="Afegir" />
+    </form>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,14 +1,95 @@
 import "./App.css";
 import React from "react";
-import Llistar from "./Llistar";
+import { useEffect, useState } from "react";
 import Afegir from "./Afegir";
 
+const ENDPOINT = "https://tc-todo-2022.herokuapp.com/todos";
+
 function App() {
+  const [tasques, setTasques] = useState([]);
+  const [task, setTask] = useState([]);
+
+  useEffect(() => {
+    fetch(ENDPOINT, {
+      method: "GET",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => setTasques(JSON.parse(JSON.stringify(json))));
+    const intervalID = setInterval(() => {
+      fetch(ENDPOINT, {
+        method: "GET",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) => response.json())
+        .then((json) => setTasques(JSON.parse(JSON.stringify(json))));
+    }, 60000);
+    return () => clearInterval(intervalID);
+  }, []);
+
+  function canviaentrada(id) {
+    fetch(`${ENDPOINT}/${id}`, {
+      method: "GET",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => setTask(JSON.parse(JSON.stringify(json))));
+    let newstatus = !task.completed;
+    console.log("modificant el registre ", id, " amb ", newstatus);
+    fetch(`${ENDPOINT}/${id}`, {
+      method: "POST",
+      body: JSON.stringify({
+        completed: newstatus,
+      }),
+    }).then((response) => response.json());
+    showtasques(false);
+  }
+  function showtasques(filter) {
+    fetch(ENDPOINT + `${filter ? "?completed=false" : ""}`, {
+      method: "GET",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => setTasques(JSON.parse(JSON.stringify(json))));
+    let newArrayDataOfOjbect = Object.values(tasques);
+    setTasques(newArrayDataOfOjbect);
+  }
+  function Tasqueta({ tasca }) {
+    return (
+      <li className={tasca.completed && "completada" ? "completada" : ""}>
+        <input
+          type="checkbox"
+          value={tasca.completed}
+          checked={tasca.completed ? "checked" : false}
+          onChange={() => canviaentrada(tasca.id)}
+        />
+        &nbsp;Title={tasca.title}
+        &nbsp;Details={tasca.details}
+      </li>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Llistar />
-        <Afegir />
+        <label>Llista de tasques</label>
+        <input
+          type="submit"
+          value="Filtrar només pendents"
+          onClick={() => showtasques(true)}
+        />
+        <ul
+          style={{
+            listStyleType: "none",
+            fontSize: "11px",
+            textAlign: "left",
+          }}
+        >
+          {tasques.map((tasca, index) => (
+            <Tasqueta key={tasca.id} tasca={tasca} />
+          ))}
+        </ul>
+        <Afegir onTascaAfegida={(tasca) => setTasques([...tasques, tasca])} />
       </header>
     </div>
   );
